a7125ca7b2497db4304f87775b096cf4b22b21a8
IsaacEffect <isaacmartinez9875@gmail.com>
2025-10-30 22:34:50 -0400

    organizing

---
+++ HotelReservation.Application/Dtos/ActualizarReservaDTO.cs
@@ -0,0 +1,9 @@
+namespace HotelReservation.Application.Dtos
+{
+    public class ActualizarReservaDTO
+    {
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public string EstadoReserva { get; set; } = "Pendiente";
+    }
+}
--- HotelReservation.Domain/Entities/Reserva.cs
+++ HotelReservation.Domain/Entities/Reserva.cs
@@ -1,8 +1,9 @@
-﻿namespace HotelReservation.Domain.Entities
+namespace HotelReservation.Domain.Entities
 {
     public class Reserva
     {
         public Guid Id { get; set; }
+        public DateTime FechaReserva { get; set; } = DateTime.UtcNow;
         public DateTime FechaInicio { get; set; }
         public DateTime FechaFin { get; set; }
         public string EstadoReserva { get; set; } = "Pendiente";
@@ -11,4 +12,5 @@
         public Guid UsuarioId { get; set; }
         public decimal Total { get; set; }
     }
+
 }
