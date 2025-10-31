f531a5df1790a556a66e20d3c4e67c1b8de13b92
IsaacEffect <isaacmartinez9875@gmail.com>
2025-10-31 03:36:13 -0400

    a

---
+++ HotelReservation.Application/Contracts/ICheckInOutService.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelReservation.Application.Contracts
+{
+    public interface ICheckInOutService
+    {
+        Task RegistrarCheckInAsync(Guid reservaId);
+        Task RegistrarCheckOutAsync(Guid reservaId);
+    }
+}
