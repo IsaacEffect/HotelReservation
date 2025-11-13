a1cd2f472d67178726bbd79af4c6a7a76a40040c
Willy Alcantara <147335048+ItzXitrax@users.noreply.github.com>
2025-11-13 16:36:43 -0400

    Eliminacion de archivos repetidos

--- HotelReservation.Application/Dtos/RoomDto.cs
+++ HotelReservation.Application/Dtos/RoomDto.cs
@@ -1,6 +1,12 @@
-﻿using HotelReservation.Domain.Entities;
-
-namespace HotelReservation.Application.DTOs
+﻿namespace HotelReservation.Application.DTOs
 {
-    public record HabitacionDto(Guid Id, int Number, Guid CategoryId, HabitacionStatus Status, decimal Price);
+    public class RoomDto
+    {
+        public int Id { get; set; }
+        public string Numero { get; set; } = string.Empty;
+        public decimal PrecioPorNoche { get; set; }
+        public int Capacidad { get; set; }
+        public int CategoriaId { get; set; }
+        public string Estado { get; set; } = "Disponible";
+    }
 }
