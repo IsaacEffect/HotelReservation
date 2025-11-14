605a7da7cbc0ddc791c3961ce7526900478c02f2
IsaacEffect <isaacmartinez9875@gmail.com>
2025-11-13 21:06:26 -0400

    arreglando errores

---
+++ HotelReservation.Application/Dtos/HabitacionDto.cs
@@ -0,0 +1,6 @@
+﻿using HotelReservation.Domain.Entities;
+
+namespace HotelReservation.Application.DTOs
+{
+    public record HabitacionDto(Guid Id, int Number, Guid CategoryId, HabitacionStatus Status, decimal Price);
+}
