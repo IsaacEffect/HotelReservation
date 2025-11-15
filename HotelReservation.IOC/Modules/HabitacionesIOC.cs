2f6aa6a77f0dd21bbe37430fef21ad00053da69e
IsaacEffect <isaacmartinez9875@gmail.com>
2025-11-15 00:46:33 -0400

    Organizando dependencias

---
+++ HotelReservation.IOC/Modules/HabitacionesIOC.cs
@@ -0,0 +1,15 @@
+﻿using HotelReservation.Domain.Interfaces;
+using HotelReservation.Persistence.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelReservation.IOC.Modules
+{
+    public static class HabitacionesIOC
+    {
+        public static void RegisterHabitaciones(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IHabitacionRepository, HabitacionRepository>();
+        }
+    }
+}
