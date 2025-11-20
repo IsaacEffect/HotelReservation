5db9e76499035450202f987356d59fffd7858295
IsaacEffect <isaacmartinez9875@gmail.com>
2025-11-20 14:24:03 -0400

    agg factura

--- HotelReservation.IOC/Modules/FacturacionIOC.cs
+++ HotelReservation.IOC/Modules/FacturacionIOC.cs
@@ -1,6 +1,8 @@
 ﻿using HotelReservation.Application.Contracts;
 using HotelReservation.Application.Services;
+using HotelReservation.Domain.Interfaces;
 using HotelReservation.Infrastructure.Services;
+using HotelReservation.Persistence.Repositories;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,6 +13,7 @@ namespace HotelReservation.IOC.Modules
         public static void RegisterFacturacion(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped<IFacturaService, FacturaService>();
+            services.AddScoped<IFacturaRepository, FacturaRepository>();
             services.AddScoped<IReporteService, ReporteService>();
             services.AddSingleton<IPdfService, PdfService>();
         }
